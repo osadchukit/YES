00384fd1c5d1afc7e7748fb5d5c1ea02ae8d13f6
osadchukit <119492029+osadchukit@users.noreply.github.com>
2023-02-07 17:45:03 +0000

    Deploying to gh-pages from @ osadchukit/YES@b9a77b25b2aaf5ed675bb3552c26dde52ceb3648 🚀

---
+++ index.0bf4e9e1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("textarea");e.addEventListener("keyup",(t=>{e.style.height="63px";let l=t.target.scrollHeight;e.style.height=`${l}px`}));
+//# sourceMappingURL=index.0bf4e9e1.js.map
---
+++ index.80d361d3.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("textarea");e.addEventListener("keyup",(function(t){e.style.height="63px";var n=t.target.scrollHeight;e.style.height="".concat(n,"px")}))}();
+//# sourceMappingURL=index.80d361d3.js.map
